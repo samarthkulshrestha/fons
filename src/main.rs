8a47c8775b72948368540bb12f39f823a2e1a233
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2025-01-24 14:48:52 +0530

    added rich output

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 use std::env;
 use rusqlite::{params, Connection, Result};
+use inline_colorization::*;
 
 #[derive(Debug)]
 struct Word {
     _id: i32,
     word: String,
     pos: String,
-    hyphenation: Option<String>,
+    _hyphenation: Option<String>,
     ipa: Option<String>,
     _etym_num: Option<i32>,
     etym_txt: String,
 }
 
 fn search(conn: &Connection, term: &str) -> Result<Word> {
-    let mut recr = conn.prepare("SELECT id, word, pos, hyphenation, ipa, etymology_number, etymology_text FROM word WHERE word = ?1;")?;
+    let mut recr = conn.prepare("SELECT id, word, pos, hyphenation, ipa,    \
+        etymology_number, etymology_text FROM word WHERE word = ?1;")?;
     let word = recr.query_row(params![term], |r| {
         Ok(Word {
             _id: r.get(0)?,
             word: r.get(1)?,
             pos: r.get(2)?,
-            hyphenation: r.get(3)?,
+            _hyphenation: r.get(3)?,
             ipa: r.get(4)?,
             _etym_num: r.get(5)?,
             etym_txt: r.get(6)?,
@@ -27,6 +29,17 @@ fn search(conn: &Connection, term: &str) -> Result<Word> {
     })?;
 
     return Ok(word);
+}
+
+fn print_def(word: &Word) {
+    print!("+ {color_bright_blue}{}{color_reset} ({color_yellow}{}{color_reset}) ",
+        word.word, word.pos);
+    match &word.ipa {
+        Some(s) => println!("[{color_cyan}{}{color_reset}]", s),
+        None => println!("")
+    }
+    println!("+ {color_bright_magenta}etymology:{color_reset} \n    {}",
+        word.etym_txt);
 }
 
 fn main() -> Result<()> {
@@ -38,11 +51,7 @@ fn main() -> Result<()> {
         eprintln!("ERROR: not enough arguments!");
     } else {
         let word = search(&conn, &args[1])?;
-        println!("word: {}", word.word);
-        println!("part of speech: {}", word.pos);
-        println!("hyphenation: {:?}", word.hyphenation);
-        println!("ipa: {:?}", word.ipa);
-        println!("etymology: {}", word.etym_txt);
+        print_def(&word);
     }
 
     Ok(())
