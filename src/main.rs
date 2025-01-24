d8dbc9368a524c05be77517bfbd76dcf3642ba9a
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2025-01-24 15:31:38 +0530

    added additional etymology sources to db
    + renamed column `etymology_number` to `etym_num`
    + renamed column `etymology_text` to `etym_txt_wiki`
    + added new `etym_txt_etym` column to db
    + un-hardcoded the db file locatio, now using envvars ($XDG_DATA_HOME)

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,17 @@ use inline_colorization::*;
 struct Word {
     _id: i32,
     word: String,
-    pos: String,
+    pos: Option<String>,
     _hyphenation: Option<String>,
     ipa: Option<String>,
     _etym_num: Option<i32>,
-    etym_txt: String,
+    etym_txt_wiki: Option<String>,
+    etym_txt_etym: Option<String>,
 }
 
 fn search(conn: &Connection, term: &str) -> Result<Word> {
     let mut recr = conn.prepare("SELECT id, word, pos, hyphenation, ipa,    \
-        etymology_number, etymology_text FROM word WHERE word = ?1;")?;
+        etym_num, etym_txt_wiki, etym_txt_etym FROM word WHERE word = ?1;")?;
     let word = recr.query_row(params![term], |r| {
         Ok(Word {
             _id: r.get(0)?,
@@ -24,7 +25,8 @@ fn search(conn: &Connection, term: &str) -> Result<Word> {
             _hyphenation: r.get(3)?,
             ipa: r.get(4)?,
             _etym_num: r.get(5)?,
-            etym_txt: r.get(6)?,
+            etym_txt_wiki: r.get(6)?,
+            etym_txt_etym: r.get(7)?,
         })
     })?;
 
@@ -32,19 +34,43 @@ fn search(conn: &Connection, term: &str) -> Result<Word> {
 }
 
 fn print_def(word: &Word) {
-    print!("+ {color_bright_blue}{}{color_reset} ({color_yellow}{}{color_reset}) ",
-        word.word, word.pos);
+    print!("+ {color_bright_blue}{}{color_reset} ",
+        word.word);
+    match &word.pos {
+        Some(s) => print!("({color_yellow}{}{color_reset}) ", s),
+        None => ()
+    }
     match &word.ipa {
         Some(s) => println!("[{color_cyan}{}{color_reset}]", s),
-        None => println!("")
+        None => ()
     }
-    println!("+ {color_bright_magenta}etymology:{color_reset} \n    {}",
-        word.etym_txt);
+    match &word.etym_txt_wiki {
+        Some(s) => println!("+ {color_bright_magenta}etymology (wiktionary):{color_reset} \n    {}", s),
+        None => println!()
+    }
+    match &word.etym_txt_etym {
+        Some(s) => println!("+ {color_bright_magenta}etymology (etymonline):{color_reset} \n    {}", s),
+        None => println!()
+    }
 }
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    let path = "/home/evil/code/fons/data/enwiktionary-etym.db";
+    let envvar_db_path = env::var("FONS_DB");
+    let envvar_xdg_data = env::var("XDG_DATA_HOME");
+    let envvar_home = env::var("HOME");
+    let path;
+
+    if envvar_db_path .is_ok() {
+        path = envvar_db_path.unwrap();
+    } else if envvar_xdg_data.is_ok() {
+        path = envvar_xdg_data.unwrap() + "/fons/fons.db";
+    } else if envvar_home.is_ok() {
+        path = envvar_home.unwrap() + "/.local/share/fons/fons.db";
+    } else {
+        path = "~/.local/share/fons/fons.db".to_string();
+    }
+
     let conn = Connection::open(path)?;
 
     if args.len() < 2 {
